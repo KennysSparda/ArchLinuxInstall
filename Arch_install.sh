600fefd66cf4f6804e01afba75ee36b83abdf53f
swygax <kennykysv@gmail.com>
2022-05-27 18:11:28 -0300

    Ajustado alguns pacotes para funcionamento do wifi e corrigido algumas linhas das funcao de mover pasta pictures

--- Arch_install.sh
+++ Arch_install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-pacstrap /mnt base base-devel linux linux-firmware grub sudo networkmanager
-genfstab -U -p /mnt /mnt/etc/fstab
+pacstrap /mnt base base-devel linux linux-firmware grub sudo networkmanager iwd dhcpcd
+genfstab -U -p /mnt >> /mnt/etc/fstab
+mv -f Pictures ScriptPosInstall.sh /mnt
 arch-chroot /mnt
