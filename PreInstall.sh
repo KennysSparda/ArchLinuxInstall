88c1f5c639b20fcaa25be0ed8112f22e01710667
swygax <kennykysv@gmail.com>
2022-06-24 02:45:52 -0300

    Corrected local to move scripts from /mnt/tmp to /mnt/home

--- PreInstall.sh
+++ PreInstall.sh
@@ -12,5 +12,5 @@ pacstrap /mnt grub sudo networkmanager iwd dhcpcd
 # Generating File System Table for correctly mount points of devices
 genfstab -U -p /mnt >> /mnt/etc/fstab
 
-mv MyI3Config.sh PosInstall.sh /mnt/tmp
+mv MyI3Config.sh PosInstall.sh /mnt/home
 arch-chroot /mnt
